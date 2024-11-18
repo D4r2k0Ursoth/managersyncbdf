d0609518e66440a3346677d188f47f164c9d9305
D4r2k0Ursoth <147214204+D4r2k0Ursoth@users.noreply.github.com>
2024-11-17 22:00:11 -0600

    newssss

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,16 +42,17 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
-        ],
 
-        's3' => [
+        ],
+           
+            's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
+            'endpoint' => 'https://' . env('AWS_DEFAULT_REGION') . '.digitaloceanspaces.com', // Endpoint de DigitalOcean
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
